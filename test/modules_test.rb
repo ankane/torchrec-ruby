c789d0f447e043f122419196b9da6608758f1c6f
Andrew Kane <andrew@ankane.org>
2022-02-28 10:29:57 -0800

    Added more module tests

--- test/modules_test.rb
+++ test/modules_test.rb
@@ -5,4 +5,27 @@ class ModulesTest < Minitest::Test
     sln = TorchRec::Modules::Activation::SwishLayerNorm.new(100)
     assert sln
   end
+
+  def test_mlp
+    batch_size = 3
+    in_size = 40
+    input = Torch.randn(batch_size, in_size)
+
+    layer_sizes = [16, 8, 4]
+    mlp_module = TorchRec::Modules::MLP::MLP.new(in_size, layer_sizes, bias: true)
+    output = mlp_module.call(input)
+    assert_equal output.shape, [batch_size, layer_sizes[-1]]
+  end
+
+  def test_perceptron
+    batch_size = 3
+    in_size = 40
+    input = Torch.randn(batch_size, in_size)
+
+    out_size = 16
+    perceptron = TorchRec::Modules::MLP::Perceptron.new(in_size, out_size, bias: true)
+
+    output = perceptron.call(input)
+    assert_equal output.shape, [batch_size, out_size]
+  end
 end
